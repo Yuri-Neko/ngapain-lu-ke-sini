b824e7659aa0b3baecfaf91c7ef0f40aac7a0d58
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-08-25 12:40:16 +0800

    New
    New

--- plugins/tools-delete.js
+++ plugins/tools-delete.js
@@ -1,13 +1,23 @@
-let handler = function (m) {
-    if (!m.quoted) throw false
+let handler = function (m, { conn, command }) {
+try {
     let { chat, fromMe, isBaileys } = m.quoted
-    if (!fromMe) throw false
-    if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
-    conn.sendMessage(chat, { delete: m.quoted.vM.key })
+    await conn.sendMessage(chat, { delete: m.quoted.vM.key })
+ } catch {
+ let key = {}
+ try {
+ 	key.remoteJid = m.quoted ? m.quoted.fakeObj.key.remoteJid : m.key.remoteJid
+	key.fromMe = m.quoted ? m.quoted.fakeObj.key.fromMe : m.key.fromMe
+	key.id = m.quoted ? m.quoted.fakeObj.key.id : m.key.id
+ 	key.participant = m.quoted ? m.quoted.fakeObj.participant : m.key.participant
+ } catch (e) {
+ 	console.error(e)
+ }
+ conn.sendMessage(m.chat, { delete: key })
+ }
 }
 handler.help = ['del', 'delete']
 handler.tags = ['tools']
 
-handler.command = /^del(ete)?$/i
+handler.command = /^del|d(elete|m)?$/i
 
 export default handler
